5ae22c138d2969beff336f9af922063063956efa
Devlin <webmasterdevlin@gmail.com>
2020-07-09 00:19:10 +0200

    updated

--- AspK8s/Controllers/WeatherForecastController.cs
+++ AspK8s/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace AspK8s.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
